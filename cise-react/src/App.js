dfb9a7531fa691506022783275db35e0831679cf
Jimmy <jimmycshaw@hotmail.co.nz>
2022-03-24 21:31:15 +1300

    Select Component Integrated

--- cise-react/src/App.js
+++ cise-react/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Countries = [
+  { label: "Albania", value: 355 },
+  { label: "Argentina", value: 54 },
+  { label: "Austria", value: 43 },
+  { label: "Cocos Islands", value: 61 },
+  { label: "Kuwait", value: 965 },
+  { label: "Sweden", value: 46 },
+  { label: "Venezuela", value: 58 }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <Select options={Countries} />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default App
